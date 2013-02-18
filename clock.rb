54ed8ca6976a8579a9fb00437bed762f8fc4a88a
Alexey <alexey.kondratenko@mail.ru>
2013-02-18 14:36:58 +0200

    change time in clock.rb 1500

--- clock.rb
+++ clock.rb
@@ -4,4 +4,4 @@ require 'clockwork'
 include Clockwork
 
 
-every(1.day, 'Queueing scheduled job', :at => '14:17') { Delayed::Job.enqueue ScheduledJob.new }
+every(1.day, 'Queueing scheduled job', :at => '15:00') { Delayed::Job.enqueue ScheduledJob.new }
