cbd610ec55728aa10b68aef6f694458fcbc743de
Alexey <alexey.kondratenko@mail.ru>
2013-03-06 16:24:16 +0200

    push heroku

--- clock.rb
+++
@@ -1,7 +0,0 @@
-﻿require File.expand_path('../realtSite.rb', __FILE__)
-require 'clockwork'
-
-include Clockwork
-
-
-every(2.minutes, 'Queueing interval job') { Delayed::Job.enqueue IntervalJob.new  }
